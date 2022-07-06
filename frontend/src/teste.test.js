40b3c5b0bbcb82bda1f2d3a4cc2623b3e833bd5a
makuntz <mkprovida@gmail.com>
2022-07-06 08:08:41 -0300

    manutencao package

---
+++ frontend/src/teste.test.js
@@ -0,0 +1,23 @@
+// import axios from "axios";
+import { request } from "http";
+// const expect  = require('chai').expect;
+////* "mocha": "^10.0.0",*/
+const assert = require('assert')
+
+describe('TESTE', function() {
+
+
+    it('testes', async function() {
+        // const options = {
+        //     method: 'GET',
+        //     url: `http://localhost:4000/listuser`
+        // }
+        
+        
+        // const users = await axios.request(options)
+
+        // console.log(users)
+
+        
+    });
+});
